34677349dc1080eb872d5f33e1d409e24ce868f4
ahmermughal <a2mughal143@gmail.com>
2022-06-12 13:48:15 +0500

    Setup WatchVC TableView

---
+++ TentwentyTakeHome/Utils/MovieColors.swift
@@ -0,0 +1,16 @@
+//
+//  MovieColors.swift
+//  TentwentyTakeHome
+//
+//  Created by Ahmer Mughal on 6/12/22.
+//
+
+import Foundation
+import UIKit
+
+
+enum MovieColors{
+    
+    static let secondaryBackgroundColor = UIColor(named: "color-background-secondary")!
+    
+}
--- TentwentyTakeHome/Watch/WatchVC.swift
+++ TentwentyTakeHome/Watch/WatchVC.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class WatchVC: UIViewController {
 
+    let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureVC()
+        configureUI()
+        layoutUI()
     }
 
 
+}
+
+extension WatchVC{
+    
+    private func configureVC(){
+        view.backgroundColor = MovieColors.secondaryBackgroundColor
+    }
+    
+    private func configureUI(){
+        tableView.backgroundColor = .clear
+    }
+    
+    
+    private func layoutUI(){
+        
+        let views = [tableView]
+        
+        for item in views{
+            item.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(item)
+        }
+        
+        NSLayoutConstraint.activate([
+        
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            
+        ])
+        
+        
+    }
+    
+    
 }
 
