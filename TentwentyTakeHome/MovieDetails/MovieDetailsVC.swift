2e5e7c6889a1e25aa2bbd1ecae5a03a9552c937f
ahmermughal <a2mughal143@gmail.com>
2022-06-12 16:17:23 +0500

    Add MovieDetailsVC

---
+++ TentwentyTakeHome/MovieDetails/MovieDetailsVC.swift
@@ -0,0 +1,38 @@
+//
+//  MovieDetailsVC.swift
+//  TentwentyTakeHome
+//
+//  Created by Ahmer Mughal on 6/12/22.
+//
+
+import UIKit
+
+class MovieDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureVC()
+        configureUI()
+        layoutUI()
+    }
+
+}
+
+extension MovieDetailsVC{
+    
+    private func configureVC(){
+        view.backgroundColor = MovieColors.secondaryBackgroundColor
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    private func configureUI(){
+        
+        
+    }
+    
+    private func layoutUI(){
+        
+        
+    }
+    
+}
